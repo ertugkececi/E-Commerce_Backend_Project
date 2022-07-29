4fd4e76538bbcf97b5131b468ad1e996201fef90
ertugkececi <89524989+ertugkececi@users.noreply.github.com>
2022-07-29 20:37:54 +0300

    Layers Created
    Create project
    Added layers.
    Added EntityFramework
    Using N-Tier Architecture

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductTest();
+CategoryTest();
+
+static void ProductTest()
+{
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    foreach (var product in productManager.GetProductDetails())
+    {
+        Console.WriteLine($"{product.ProductName} - {product.CategoryName}");
+    }
+    Console.WriteLine("---------------------------\n");
+}
+
+static void CategoryTest()
+{
+    CategoryManager CategoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in CategoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
+    Console.WriteLine("---------------------------\n");
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // User Id = SA; Password=Abartmayin12.
+            optionsBuilder.UseSqlServer(@"Server=localhost; Database= NorthWindDB;User Id = SA; Password=Abartmayin12.");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
+
