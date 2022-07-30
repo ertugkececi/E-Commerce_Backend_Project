3619ccd4ad7c2d6e30a14fac1b20299521aa0c81
etocans <ertugkececi@gmail.com>
2022-07-31 00:10:30 +0300

    Result yapısı eklendi
    Core katmanına result dönüş utilities'i eklendi.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "ürün ismi geçersiz.";
+        public static string MaintanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi.";
+    }
+}
+
